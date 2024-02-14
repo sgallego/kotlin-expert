67eeef387420128994ae97e7b85f75fea9bb4ac1
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-02-14 12:15:47 +0100

    Practice 2.6

--- composeApp/src/desktopMain/kotlin/App.kt
+++ composeApp/src/desktopMain/kotlin/App.kt
@@ -5,25 +5,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 
 @Composable
 @Preview
-fun App() {
-    val text = remember { mutableStateOf("") }
-    val buttonEnabled = text.value.isNotEmpty()
+fun App(state: AppState) {
     fun buildMessage(text: String): String{
         return "Hello $text"
     }
 
     MaterialTheme {
         Column {
-            TextField(value = text.value, onValueChange = { newText -> text.value = newText })
-            Text(text = buildMessage(text.value))
+            TextField(value = state.text.value, onValueChange = { newText -> state.text.value = newText })
+            Text(text = buildMessage(state.text.value))
             Button(onClick = {
-                text.value = ""
-            }, enabled = buttonEnabled) {
+                state.text.value = ""
+            }, enabled = state.buttonEnabled()) {
                 Text("Clean")
             }
         }
--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,16 +1,25 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "KotlinExpertNotesDemo") {
-        App()
+fun main() {
+    val state = AppState()
+    application {
+        Window(onCloseRequest = ::exitApplication, title = "KotlinExpertNotesDemo") {
+            App(state)
+        }
     }
+}
+
+class AppState(){
+    val text = mutableStateOf("")
+    fun buttonEnabled() = text.value.isNotEmpty()
 }
 
 @Preview
 @Composable
 fun AppDesktopPreview() {
-    App()
+    App(AppState())
 }
