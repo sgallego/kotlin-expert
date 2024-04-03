e1931979f541af1e193437e58909a0905f85e107
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-04-03 11:34:06 +0200

    8.4 Base web UI with Compose Web

--- common/src/commonMain/kotlin/com/sgallego/kotlinexpert/data/remote/NotesClient.kt
+++ common/src/commonMain/kotlin/com/sgallego/kotlinexpert/data/remote/NotesClient.kt
@@ -1,11 +1,10 @@
 package com.sgallego.kotlinexpert.data.remote
 
 import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 
-val notesClient = HttpClient(OkHttp){
+val notesClient = HttpClient(){
     install(ContentNegotiation){
         json()
     }
--- web/src/jsMain/kotlin/Main.kt
+++ web/src/jsMain/kotlin/Main.kt
@@ -1,33 +1,50 @@
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import com.sgallego.kotlinexpert.data.Note
 import com.sgallego.kotlinexpert.getAppTitle
+import com.sgallego.kotlinexpert.ui.screens.home.HomeViewModel
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    var count: Int by mutableStateOf(0)
 
     renderComposable(rootElementId = "root") {
 
-        Div({ style { padding(25.px) } }) {
-            H1 { Text(getAppTitle()) }
-            Button(attrs = {
-                onClick { count -= 1 }
-            }) {
-                Text("-")
-            }
+        val scope = rememberCoroutineScope()
+        val homeViewModel = remember { HomeViewModel(scope) }
 
-            Span({ style { padding(15.px) } }) {
-                Text("$count")
-            }
+        NotesList(homeViewModel.state.filteredNotes ?: emptyList()){ note ->
+            console.log("Note clicked: $note")
 
-            Button(attrs = {
-                onClick { count += 1 }
-            }) {
-                Text("+")
+        }
+
+    }
+}
+
+@Composable
+fun NotesList(notes: List<Note>, onNoteClick: (Note) -> Unit){
+    Div{
+        notes.forEach {  note ->
+            NoteCard(note, onNoteClick)
+        }
+    }
+}
+
+@Composable
+fun NoteCard(note: Note, onNoteClick: (Note) -> Unit){
+    Div(
+        attrs = {
+            onClick {
+                onNoteClick(note)
             }
+        }
+    ) {
+        H3 { Text(note.title) }
+        if(note.type == Note.Type.AUDIO){
+            Span { Text("🎤") }
+        }
+        Div {
+            Text(note.description)
         }
     }
 }
