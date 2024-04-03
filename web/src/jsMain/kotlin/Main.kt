f14259fd27580cd210aa7323686cb8afe041ca80
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-04-03 18:08:57 +0200

    8.11 Kotlin/JS script example

--- web/src/jsMain/kotlin/Main.kt
+++ web/src/jsMain/kotlin/Main.kt
@@ -1,14 +1,30 @@
 
+import kotlinx.browser.document
+import kotlinx.browser.window
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
 import ui.App
 import ui.theme.AppStyleSheet
 
 fun main() {
+    composeSample()
+    kotlinJsSample()
+}
 
+private fun composeSample(){
+    document.getElementById("root") ?: return
     renderComposable(rootElementId = "root") {
         Style(AppStyleSheet)
         App()
     }
 }
 
+fun kotlinJsSample(){
+    val message = document.getElementById("message") ?: return
+    message.textContent = "Hello Kotlin/JS"
+
+    val button = document.getElementById("button") ?: return
+    button.addEventListener("click", {
+        window.alert("You clicked the button")
+    })
+}
