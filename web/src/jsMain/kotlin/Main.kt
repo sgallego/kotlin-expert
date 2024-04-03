c67c2ad0a58c7bfe30d95d98930ddfe89f1cab57
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-04-03 11:46:06 +0200

    8.5 Card style

--- web/src/jsMain/kotlin/Main.kt
+++ web/src/jsMain/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import androidx.compose.runtime.*
 import com.sgallego.kotlinexpert.data.Note
-import com.sgallego.kotlinexpert.getAppTitle
 import com.sgallego.kotlinexpert.ui.screens.home.HomeViewModel
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
@@ -23,7 +22,16 @@ fun main() {
 
 @Composable
 fun NotesList(notes: List<Note>, onNoteClick: (Note) -> Unit){
-    Div{
+    Div(attrs = {
+        style {
+            display(DisplayStyle.Flex)
+            flexDirection(FlexDirection.Column)
+            gap(16.px)
+            width(100.percent)
+            height(100.percent)
+            alignItems(AlignItems.Center)
+        }
+    }){
         notes.forEach {  note ->
             NoteCard(note, onNoteClick)
         }
@@ -34,15 +42,43 @@ fun NotesList(notes: List<Note>, onNoteClick: (Note) -> Unit){
 fun NoteCard(note: Note, onNoteClick: (Note) -> Unit){
     Div(
         attrs = {
-            onClick {
-                onNoteClick(note)
+            onClick { onNoteClick(note) }
+            style {
+                display(DisplayStyle.Flex)
+                flexDirection(FlexDirection.Column)
+                width(80.percent)
+                maxWidth(600.px)
+                marginTop(8.px)
+                marginBottom(8.px)
+                padding(16.px)
+                border(1.px, LineStyle.Solid, Color.black)
+                borderRadius(4.px)
+                cursor("pointer")
             }
         }
     ) {
-        H3 { Text(note.title) }
-        if(note.type == Note.Type.AUDIO){
-            Span { Text("🎤") }
+        Div(
+            attrs = {
+                style {
+                    display(DisplayStyle.Flex)
+                    flexDirection(FlexDirection.Row)
+                    alignItems(AlignItems.Center)
+                    width(100.percent)
+                }
+            }
+        ) {
+            H3(
+                attrs = {
+                    style {
+                        flex(1)
+                    }
+                }
+            ) { Text(note.title) }
+            if(note.type == Note.Type.AUDIO){
+                Span { Text("🎤") }
+            }
         }
+
         Div {
             Text(note.description)
         }
