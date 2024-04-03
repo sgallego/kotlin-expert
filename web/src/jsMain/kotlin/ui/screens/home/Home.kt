3d0468962a1477e6a74646e64f926bf0a8c32ad6
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-04-03 12:50:01 +0200

    8.8 Home UI update

--- web/src/jsMain/kotlin/ui/screens/home/Home.kt
+++ web/src/jsMain/kotlin/ui/screens/home/Home.kt
@@ -1,11 +1,48 @@
 package ui.screens.home
 
 import androidx.compose.runtime.Composable
+import com.sgallego.kotlinexpert.data.Note
 import com.sgallego.kotlinexpert.ui.screens.home.HomeViewModel
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Text
+import ui.theme.AppStyleSheet
 
 @Composable
 fun Home(vm: HomeViewModel, onNoteClick: (noteId: Long) -> Unit){
-    NotesList(vm.state.filteredNotes ?: emptyList()){ note ->
-        onNoteClick(note.id)
+
+    Div(attrs = {
+        style {
+            display(DisplayStyle.Flex)
+            flexDirection(FlexDirection.Column)
+            width(100.percent)
+            height(100.percent)
+        }
+    }) {
+        TopBar(onFilterClick = vm::onFilterClick)
+
+        Div{
+            if(vm.state.loading){
+                Text("Cargando...")
+            }
+
+            vm.state.filteredNotes?.let { notes ->
+                NotesList(
+                    notes = notes,
+                    onNoteClick = { onNoteClick(it.id) }
+                )
+            }
+        }
+
+        Div( attrs = {
+            classes(AppStyleSheet.fab)
+            onClick {
+                onNoteClick(Note.NEW_NOTE)
+            }
+        }) {
+            Text("+")
+        }
     }
+
+
 }
---
+++ web/src/jsMain/kotlin/ui/screens/home/TopBar.kt
@@ -0,0 +1,64 @@
+package ui.screens.home
+
+import androidx.compose.runtime.*
+import com.sgallego.kotlinexpert.data.Filter
+import com.sgallego.kotlinexpert.data.Note
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.color
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.H1
+import org.jetbrains.compose.web.dom.Text
+import ui.theme.AppStyleSheet
+
+@Composable
+fun TopBar(onFilterClick:  (Filter) -> Unit) {
+    Div(attrs = {
+        classes(AppStyleSheet.topBar)
+    }){
+        H1(attrs = {
+            classes(AppStyleSheet.topBarTitle)
+        }){ Text("Mis notas")   }
+        FiltersActions(onFilterClick)
+    }
+}
+
+@Composable
+fun FiltersActions(onFilterClick: (Filter) -> Unit){
+    var expanded by remember { mutableStateOf(false) }
+
+    Div(attrs = {
+        classes(AppStyleSheet.filtersAction)
+    }) {
+        Div(attrs = {
+            style {
+                color(Color.white)
+            }
+            onClick { expanded = true }
+        }){
+            Text("🔎")
+        }
+        if(expanded){
+            Div(attrs = {
+                classes(AppStyleSheet.filtersActionExpanded)
+            }) {
+                listOf<Pair<Filter, String>>(
+                    Filter.All to "All",
+                    Filter.ByType(Note.Type.TEXT) to "Text",
+                    Filter.ByType(Note.Type.AUDIO) to "Audio",
+                ).forEach { (filter, label) ->
+                    Div(attrs = {
+                        classes(AppStyleSheet.filtersActionExpandedItem)
+                        onClick {
+                            onFilterClick(filter)
+                            expanded = false
+                        }
+                    }) {
+                        Text(label)
+                    }
+                }
+            }
+
+
+        }
+    }
+}
